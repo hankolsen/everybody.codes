72f5cffce7b444d0fbae1e6368105748a15b8dc1
Hank <hank@mullvad.net>
2024-11-14 12:13:47 +0100

    Add 2024-08 part2

--- 2024/08/index.ts
+++ 2024/08/index.ts
@@ -8,4 +8,22 @@ const part1 = () => {
   console.log({ part1: diff * width });
 };
 
+const part2 = () => {
+  const input = "998";
+  const priestsOfNull = Number(input);
+  const priestsAcolytes = 1111;
+  const maxBlocks = 20240000;
+
+  let thickness = 1;
+  let width = 1;
+  let usedBlocks = 1;
+  while (usedBlocks < maxBlocks) {
+    thickness = (thickness * priestsOfNull) % priestsAcolytes;
+    usedBlocks += (width + 2) * thickness;
+    width += 2;
+  }
+  console.log({ part2: (usedBlocks - maxBlocks) * width });
+};
+
 part1();
+part2();
